fc451c0458a983d9c707e6c7f7057446f5af3a08
Taiga-422 <taigakun0422@gmail.com>
2024-11-15 09:53:10 +0900

    ファイルの一次アップロード

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// BOSSのsaveボタンクリック時のイベント
+$("#boss-save").on("click", function () {
+    const bossKey = $("#boss-num").val();
+    const bossValue = $("#boss-memo").val();
+
+    localStorage.setItem(bossKey, bossValue);
+    const html = `
+    <ul>
+        <li >${bossKey}</li>
+        <li >${bossValue}</li>
+    </ul>
+    `;
+    $("#boss-memoarea").append(html);
+});
+
+// WORKERのsaveボタンクリック時のイベント
+$("#worker-save").on("click", function () {
+    const workerKey = $("#worker-num").val();
+    const workerValue = $("#worker-memo").val();
+
+    localStorage.setItem(workerKey, workerValue);
+    const html = `
+    <ul>
+        <li >${workerKey}</li>
+        <li >${workerValue}</li>
+    </ul>
+    `;
+    $("#worker-memoarea").append(html);
+});
+
+// clearクリック時のイベント
+$("#boss-clear").on("click", function () {
+    let bossNum = $("#boss-num");
+    bossNum.val("");
+    let bossMemo = $("#boss-memo");
+    bossMemo.val("");
+});
+
+// clearクリック時のイベント
+$("#worker-clear").on("click", function () {
+    let workerNum = $("#worker-num");
+    workerNum.val("");
+    let workerMemo = $("#worker-memo");
+    workerMemo.val("");
+});
+
+// BOSS側のページを読み込み、保存したデータ取得して表示する挙動
+for (let i = 0; i < localStorage.length; i++) {
+    const bossKey = localStorage.bossKey(i);
+    const bossValue = localStorage.getItem(bossKey);
+    const html = `
+      <tr>
+        <td>${bossKey}</td>
+        <td>${bossValue}</td>
+      </tr>
+      `;
+    $("#boss-memoarea").append(html);
+};
+
+// deleteクリック時のイベント ※課題提出後に改良。
+$("#boss-delete").on("click", function () {
+    localStorage.clear();
+    $("#boss-memoarea").empty();
+});
+
+
